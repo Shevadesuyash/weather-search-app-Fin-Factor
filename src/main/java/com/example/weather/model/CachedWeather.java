b3fdf7a8c0e88a704240283551a95b459f385f7d
suyash <shevadesuyash@gmail.com>
2026-01-01 12:41:33 +0530

    Complete Working Code

--- src/main/java/com/example/weather/model/CachedWeather.java
+++ src/main/java/com/example/weather/model/CachedWeather.java
@@ -6,15 +6,15 @@ import lombok.Data;
 @Data
 public class CachedWeather {
 
-    private WeatherResponse weatherResponse;
-    private Instant timestamp;
+  private WeatherResponse weatherResponse;
+  private Instant timestamp;
 
-    public CachedWeather(WeatherResponse weatherResponse) {
-        this.weatherResponse = weatherResponse;
-        this.timestamp = Instant.now();
-    }
+  public CachedWeather(WeatherResponse weatherResponse) {
+    this.weatherResponse = weatherResponse;
+    this.timestamp = Instant.now();
+  }
 
-    public boolean isExpired(long expirySeconds) {
-        return Instant.now().isAfter(timestamp.plusSeconds(expirySeconds));
-    }
+  public boolean isExpired(long expirySeconds) {
+    return Instant.now().isAfter(timestamp.plusSeconds(expirySeconds));
+  }
 }
