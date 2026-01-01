beba6ef11c4ff270925ee52d5c14feb76bc93a4e
suyash <shevadesuyash@gmail.com>
2026-01-01 09:17:22 +0530

    initial Commit

---
+++ src/main/java/com/example/weather/WeatherSearchAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSearchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSearchAppApplication.class, args);
+	}
+
+}
